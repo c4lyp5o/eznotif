2cb6c212d644cc18e6883b18cde0fdd476b98ad9
c4lyp5o <calypso@calypsocloud.one>
2026-02-09 12:11:11 +0800

    fix: sw options

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,15 +1,18 @@
 self.addEventListener("push", (event) => {
-	const data = event.data.json();
+	if (!(self.Notification && self.Notification.permission === "granted")) {
+		return;
+	}
+
+	const data = event.data?.json() ?? {};
 
 	const title = data.title || "New Notification";
-
 	const options = {
 		body: data.message || "You have a new update!",
-		icon: "/icon.png",
-		badge: "/badge.png",
-		tag: "simple-push-demo-notification",
-		vibrate: [200, 100, 200],
-		renotify: true,
+		// tag: "simple-push-demo-notification",
+		// icon: "/icon.png",
+		// badge: "/badge.png",
+		vibrate: [200, 100, 200, 100, 200, 100, 200],
+		// renotify: true,
 	};
 
 	event.waitUntil(self.registration.showNotification(title, options));
