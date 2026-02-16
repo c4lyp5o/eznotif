ec74dc058ee4b12e483d00e0be7646bee7635a84
c4lyp5o <calypso@calypsocloud.one>
2026-02-17 01:40:01 +0800

    fix: use lowdb

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import path from "node:path";
 import express from "express";
 import cors from "cors";
 import webpush from "web-push";
+import { Low } from "lowdb";
+import { JSONFile } from "lowdb/node";
 
 import notFound from "./middlewares/notFound.js";
 import errorHandler from "./middlewares/errorHandler.js";
@@ -20,7 +22,14 @@ const webpushOptions = {
 	urgency: "high",
 };
 
-const subscriptions = new Set();
+const file = path.join(process.cwd(), "db", "db.json");
+const adapter = new JSONFile(file);
+const db = new Low(adapter, { subscriptions: [] });
+
+// Read data immediately
+await db.read();
+db.data ||= { subscriptions: [] };
+await db.write();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -32,34 +41,51 @@ app.use(cors());
 
 app.get("/api/v1/subs", (_req, res) => {
 	// console.log("Fetching subscriptions");
-	res.json(Array.from(subscriptions));
+	res.json(db.data.subscriptions);
 });
 
-app.post("/api/v1/subs", (req, res) => {
+app.post("/api/v1/subs", async (req, res) => {
 	// console.log("Adding subscription");
-	const { body } = req;
-	subscriptions.add(body);
+	const subPayload = req.body;
+	if (!subPayload.subscription?.endpoint) {
+		return res.status(400).json({ message: "Invalid subscription payload" });
+	}
+
+	const exists = db.data.subscriptions.find(
+		(s) => s.subscription.endpoint === subPayload.subscription.endpoint,
+	);
+
+	if (!exists) {
+		db.data.subscriptions.push(subPayload);
+		await db.write();
+	} else {
+		return res.status(409).json({ message: "Subscription already exists" });
+	}
+
 	res.status(201).json({});
 });
 
-app.delete("/api/v1/subs", (req, res) => {
+app.delete("/api/v1/subs", async (req, res) => {
 	// console.log("Removing subscription");
 	const { name } = req.body;
-	const subscription = Array.from(subscriptions).find(
-		(sub) => sub.name === name,
+	const initialLength = db.data.subscriptions.length;
+	db.data.subscriptions = db.data.subscriptions.filter(
+		(sub) => sub.name !== name,
 	);
-	if (!subscription) {
-		return res.status(404).json({ message: "Subscription not found" });
+
+	if (db.data.subscriptions.length < initialLength) {
+		await db.write();
+		return res.json({ message: "Deleted" });
 	}
 
-	subscriptions.delete(subscription);
-	res.json({});
+	res.status(404).json({ message: "Subscription not found" });
 });
 
-app.delete("/api/v1/clear-subs", (_req, res) => {
+app.delete("/api/v1/clear-subs", async (_req, res) => {
 	// console.log("Clearing all subscriptions");
-	subscriptions.clear();
-	res.json({});
+	db.data.subscriptions = [];
+	await db.write();
+	res.json({ message: "All subscriptions cleared" });
 });
 
 app.post("/api/v1/notify", async (req, res) => {
@@ -67,24 +93,38 @@ app.post("/api/v1/notify", async (req, res) => {
 	const { title, message } = req.body;
 	const payload = JSON.stringify({ title, message });
 
-	const sendNotifications = Array.from(subscriptions).map((sub) =>
+	let subsCleanupNeeded = false;
+
+	const sendNotifications = db.data.subscriptions.map((sub) =>
 		webpush
 			.sendNotification(sub.subscription, payload, webpushOptions)
 			.catch((err) => {
 				if (err.statusCode === 410 || err.statusCode === 404) {
-					subscriptions.delete(sub);
+					// Mark for cleanup
+					console.log(`Endpoint expired: ${sub.subscription.endpoint}`);
+					sub._deleteMe = true;
+					subsCleanupNeeded = true;
+				} else {
+					console.error("WebPush Error:", err);
 				}
 			}),
 	);
 
 	await Promise.all(sendNotifications);
-	res.json({ message: "Notifications sent" });
+
+	if (subsCleanupNeeded) {
+		// Clean up dead subscriptions
+		db.data.subscriptions = db.data.subscriptions.filter((s) => !s._deleteMe);
+		await db.write();
+	}
+
+	res.json({ message: "Notifications processed" });
 });
 
 app.post("/api/v1/notifyone", async (req, res) => {
 	// console.log("Sending notification to one user", req.body);
 	const { name } = req.body;
-	const singleSub = Array.from(subscriptions).find((sub) => sub.name === name);
+	const singleSub = db.data.subscriptions.find((sub) => sub.name === name);
 	if (!singleSub) {
 		return res.status(404).json({ message: "Subscription not found" });
 	}
