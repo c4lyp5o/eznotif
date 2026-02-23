be48a316ba87b9532cdb72beec2fd996e513047c
c4lyp5o <calypso@calypsocloud.one>
2026-02-23 22:44:59 +0800

    chore: arrangement

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { ToastProvider } from "./context/ToastContext.jsx";
 import App from "./App.jsx";
+
+import "./index.css";
 
 if ("serviceWorker" in navigator) {
 	window.addEventListener("load", () => {
